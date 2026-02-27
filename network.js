f8d5e2522a985b2d6f44039ad0d29003d38ae0e9
Husnain Tahir <sumailjunior03@gmail.com>
2026-02-27 23:50:53 +0500

    updated

--- network.js
+++ network.js
@@ -7,6 +7,7 @@ window.CALCHQ_NETWORK = [
   { name: "BankCutoffChecker.com", url: "https://bankcutoffchecker.com" },
   { name: "SalaryVsInflation.com", url: "https://salaryvsinflation.com" },
   { name: "hourly2salarycalc.com", url: "https://hourly2salarycalc.com" },
+  { name: "TokenDollarMargin.com", url: "https://tokentodollarmargin.com" },
 ];
 
 (function () {

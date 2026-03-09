b181a59a60d30c32b0452ed045412ec93fa60169
sumailjunior03-ui <sumailjunior03@gmail.com>
2026-03-09 20:46:04 +0500

    updated

--- network.js
+++ network.js
@@ -15,7 +15,6 @@ window.CALCHQ_NETWORK = [
   { name: "TotalCompCalc.com",      url: "https://totalcompcalc.com" },
   { name: "OvertimePayCalc.com",    url: "https://overtimepaycalc.com" },
   { name: "AfterTaxSalaryCalc.com", url: "https://aftertaxsalarycalc.com" },
-  { name: "TakeHomePayCalc.com",   url: "https://takehomepaycalc.com" },
 ];
 
 (function () {
